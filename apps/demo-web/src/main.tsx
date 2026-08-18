8a2d48988153e85e7c8403ea1c1d37aeb55b0a8a
Kevin Ku <kku1993dev@gmail.com>
2026-08-18 11:17:06 -0700

    Load demo-web local css

--- apps/demo-web/src/main.tsx
+++ apps/demo-web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 import "@kku1993/buy-me-a-cup-of-tea/styles.css";
+import "./style.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
