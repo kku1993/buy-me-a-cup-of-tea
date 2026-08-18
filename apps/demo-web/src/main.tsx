b919647b8ed159b0b91707b15aa322dea65ea167
Kevin Ku <kku1993dev@gmail.com>
2026-08-18 11:13:51 -0700

    Fix css on demo site

--- apps/demo-web/src/main.tsx
+++ apps/demo-web/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import "./style.css";
+
+import "@kku1993/buy-me-a-cup-of-tea/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
